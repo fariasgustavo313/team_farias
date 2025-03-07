bd8cfd6782a93c451185288cbff8db5dd91d5c51
Gustavo <fariasgustavo313@gmail.com>
2025-03-07 15:10:54 -0300

    agregado de imagenes y videos a la galeria

--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -11,13 +11,26 @@ import { Component } from '@angular/core';
 })
 export class GaleriaComponent {
 
-  imagenes = [
+  contenido = [
 
-    { src: "img1.jpeg", alt: "imagen de kickboxing" },
-    { src: "img2.jpeg", alt: "imagen de kickboxing" },
-    { src: "img3.jpeg", alt: "imagen de kickboxing" },
-    { src: "img4.jpeg", alt: "imagen de kickboxing" },
-    { src: "img5.jpeg", alt: "imagen de kickboxing" },
+    { src: "/img/img1.jpeg", alt: "imagen de kickboxing", tipo: "imagen" },
+    { src: "img/img2.jpeg", alt: "imagen de kickboxing", tipo: "imagen" },
+    { src: "img/img3.jpeg", alt: "imagen de kickboxing", tipo: "imagen" },
+    { src: "img/img4.jpeg", alt: "imagen de kickboxing", tipo: "imagen" },
+    { src: "img/img5.jpeg", alt: "imagen de kickboxing", tipo: "imagen" },
+    { src: "img/img6.jpeg", alt: "imagen de kickboxing", tipo: "imagen" },
+    { src: "img/img7.jpeg", alt: "imagen de kickboxing", tipo: "imagen" },
+    { src: "img/img8.jpeg", alt: "imagen de kickboxing", tipo: "imagen" },
+    { src: "video/video1.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video2.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video3.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video4.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video5.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video6.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video7.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video8.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video9.mp4", alt: "video de kickboxing", tipo: "video" },
+    { src: "video/video10.mp4", alt: "video de kickboxing", tipo: "video" },
   ]
 
 }
